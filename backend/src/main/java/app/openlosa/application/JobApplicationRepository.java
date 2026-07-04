4620512e64c88fd28de3caee6edd8fca99aab061
dzkchen <102616038+dzkchen@users.noreply.github.com>
2026-07-04 00:27:25 -0400

    N+1 change (those insta reels paid off)

--- backend/src/main/java/app/openlosa/application/JobApplicationRepository.java
+++ backend/src/main/java/app/openlosa/application/JobApplicationRepository.java
@@ -1,7 +1,16 @@
 package app.openlosa.application;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long>, JpaSpecificationExecutor<JobApplication> {
+
+    @Override
+    @EntityGraph(attributePaths = "company")
+    List<JobApplication> findAll(Specification<JobApplication> spec, Sort sort);
 }
